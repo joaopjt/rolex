ca9590756ecccad291e5082f1ba3d881441e1414
João Pedro <joaopjt@icloud.com>
2024-03-04 18:59:43 +0100

    UPDATE: organized hours into files

---
+++ _time.js
@@ -0,0 +1,41 @@
+import zero_hour from './_hours/_00.json';
+import hour_one from './_hours/_01.json';
+import hour_two from './_hours/_02.json';
+import hour_three from './_hours/_03.json';
+import hour_four from './_hours/_04.json';
+import hour_five from './_hours/_05.json';
+import hour_six from './_hours/_06.json';
+import hour_seven from './_hours/_07.json';
+import hour_eight from './_hours/_08.json';
+import hour_nine from './_hours/_09.json';
+import hour_ten from './_hours/_10.json';
+import hour_eleven from './_hours/_11.json';
+import hour_twelve from './_hours/_12.json';
+import hour_thirteen_to_eighteen from './_hours/_13-to-18.json';
+import hour_nineteen from './_hours/_19.json';
+import hour_twenty from './_hours/_20.json';
+import hour_twenty_one from './_hours/_21.json';
+import hour_twenty_two from './_hours/_22.json';
+import hour_twenty_three from './_hours/_23.json';
+
+export const time = {
+  '00': zero_hour,
+  '01': hour_one,
+  '02': hour_two,
+  '03': hour_three,
+  '04': hour_four,
+  '05': hour_five,
+  '06': hour_six,
+  '07': hour_seven,
+  '08': hour_eight,
+  '09': hour_nine,
+  '10': hour_ten,
+  '11': hour_eleven,
+  '12': hour_twelve,
+  '13/18': hour_thirteen_to_eighteen,
+  '19': hour_nineteen,
+  '20': hour_twenty,
+  '21': hour_twenty_one,
+  '22': hour_twenty_two,
+  '23': hour_twenty_three
+}
